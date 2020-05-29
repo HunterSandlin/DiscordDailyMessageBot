349d91e54af5c28c9fd138a8ba538e682f727113
Hunter Sandlin <hsandlin@yahoo.com>
2020-05-28 20:32:21 -0400

    Added basic commands and user array

--- DailyMessageBot.py
+++ DailyMessageBot.py
@@ -1,17 +1,43 @@
 import discord
+from discord.ext import commands
+TOKEN = 'COPY_TOKEN_HERE'
+
 #connect to discord 
 client = discord.Client()
+#set command prefix
+client = commands.Bot(command_prefix='dmb ')
 
+users = []
+
+#--------Handles Events---------
 @client.event
-async def on_message(message):
-    message.content.lower()
-    #make sure message is not from this bot
-    if message.author == client.user:
-        return
-    if message.content.startswith("hello"):
-        await message.channel.send("Hello! I am a bot.")
+async def on_ready():
+    print("Bot is ready.")
 
 
-#run bot, string is the bot token from discord developer website    
-client.run('DISCORD_BOT_TOKEN')
+#---------Handles Commands---------
+@client.command()
+async def ping(ctx):
+    await discord.abc.Messageable.send(ctx, "pong")
+
+@client.command()
+async def echo(ctx, *args):
+    output = ''
+    for word in args:
+        output += word + ' '
+    await discord.abc.Messageable.send(ctx, output)
+
+@client.command()
+async def addUser(ctx, *newUsers):
+    for user in newUsers:
+        #remove extra parts of id, comes as <@!555>
+        user = user.replace("<@!","")
+        user = user.replace(">","")
+        #get the user from the server and add to users array
+        user = ctx.message.guild.get_member(int(user))
+        users.append(user)
+    await discord.abc.Messageable.send(ctx, "Users added.")
+
+#run bot using token    
+client.run(TOKEN)
 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# DiscordDailyMessageBot
+# Discord Daily Message Bot
+This is a Discord bot for sending a daily message to a user using discord.py
