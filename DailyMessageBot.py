968670e7a2125edc4d3db7e3a18057f3ffdc7097
Hunter Sandlin <hsandlin@yahoo.com>
2020-05-30 14:41:03 -0400

    Replaced message with embed and made it send through DM

--- DailyMessageBot.py
+++ DailyMessageBot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import yaml
 import random
+from datetime import datetime as dt
 
 with open('config.yaml', 'r') as file:
     configFile = yaml.safe_load(file)
@@ -13,6 +14,7 @@ client = discord.Client()
 client = commands.Bot(command_prefix='dmb ')
 #users that will receive messages
 users = []
+numTasks = 2
 
 #--------Handles Events---------
 @client.event
@@ -48,10 +50,13 @@ async def addUser(ctx, *newUsers):
     await discord.abc.Messageable.send(ctx, reply)
 
 @client.command()
-async def testMessage(message):
-    for user in users:
-        await user.create_dm()
-        await user.dm_channel.send("This is a test, please disregard")
+async def testTasks(ctx):
+    #if there are no users you cannot send tasks
+    if len(users) == 0:
+        await discord.abc.Messageable.send(ctx, "--There are no users added.")
+        return
+    await dmTasks(ctx)
+    await discord.abc.Messageable.send(ctx, "--DM's sent")
 
 #sends each task in a list
 @client.command()
@@ -69,15 +74,21 @@ async def viewTasks(ctx):
 #sends 2 random tasts
 @client.command()
 async def sendTasks(ctx):
-    newTasks = await getRandomTasks(2)
+    newTasks = await getRandomTasks()
     for items in newTasks:
         await discord.abc.Messageable.send(ctx, items)
 
-
+#TODO: add user
+#TODO: remove user
+#TODO: add task
+#TODO: remove task
+#TODO: update tasks per day
+#TODO: update send time
+#TODO: make help command
 
 #---------General Functions---------
 #randomly selects 'numTasks' number of tasks from yaml file, weighted
-async def getRandomTasks(numTasks):
+async def getRandomTasks():
     hat = []
     #each list of items (each 'items' is an int from config)
     for items in tasks:
@@ -93,6 +104,36 @@ async def getRandomTasks(numTasks):
         if ranChoice not in picks:
             picks.append(ranChoice)
     return picks
+
+async def dmTasks(ctx):
+    todaysTasks = await getRandomTasks()
+    todaysDate = await custom_strftime('%A, %B {S}.', dt.now())
+    todaysMessage = "\n\nGoodmorning! Today is " + todaysDate + "\n" + "Here are your tasks for today!\n"
+    itemCount = 1
+    embed = discord.Embed(
+        title = 'Tasks for ' + todaysDate,
+        description = todaysMessage,
+        color = 1752220
+    )
+    # add feild for each task
+    for items in todaysTasks:
+        embed.add_field(name="Task " + str(itemCount) + " ", value=items, inline=False)
+        itemCount += 1
+    #send message to each user  
+    for user in users:
+        await user.create_dm()
+        await user.dm_channel.send(embed=embed)
+   
+
+
+#methods for getting suffix in date, ex "May 10th"
+#decides what suffix to use
+async def suffix(d):
+        return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
+
+#gets date and formats the string
+async def custom_strftime(format, t):
+    return t.strftime(format).replace('{S}', str(t.day) + await suffix(t.day))
 
 
 #run bot using token    
